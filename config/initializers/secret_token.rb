a77cf172ae8043d7a7d172ae261aa36a9be23ed3
Chyld Medford <chyld.medford@gmail.com>
2012-09-28 12:58:54 -0400

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Io::Application.config.secret_token = '340e73192d7c8d4912ef3b52a649c087911c9a6ad45c4dd0f85a4a84bfa4c36adda7d3eabb7fa461bf23fc9b67a7a1f7b50bc05f8933b741e35b0935f6d48268'
